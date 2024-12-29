1a027d6bc98de0010f21df8821bf28f981de5802
sivog <sivograkovkirill@mail.ru>
2024-12-30 00:50:00 +0300

    init project

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const dotenv = require('dotenv-webpack');
+
+module.exports = {
+   entry: path.resolve(__dirname, 'src/index.tsx'),
+   devtool: 'inline-source-map',
+   output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'build'),
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx|ts|tsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader', 'ts-loader'],
+         },
+         {
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+         },
+         {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            use: [
+               {
+                  loader: 'file-loader',
+                  options: {
+                     name: '[name].[ext]',
+                     outputPath: 'images',
+                  },
+               },
+            ],
+         },
+      ],
+   },
+   devServer: {
+      static: {
+         directory: path.join(__dirname, 'build'),
+      },
+      port: 3001,
+      historyApiFallback: true, // Добавьте эту строку
+   },
+   resolve: {
+      fallback: {
+         process: require.resolve('process/browser'),
+      },
+      plugins: [
+         new TsconfigPathsPlugin({
+            configFile: path.join(__dirname, './tsconfig.json'),
+         }),
+      ],
+      alias: {
+         '@/*': path.resolve(__dirname, 'src'),
+      },
+      extensions: ['.*', '.js', '.jsx', '.ts', '.tsx'],
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         title: 'Youtube',
+         template: path.join(__dirname, 'public', 'index.html'),
+      }),
+      new dotenv(),
+   ],
+};
